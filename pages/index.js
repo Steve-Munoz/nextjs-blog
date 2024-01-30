3cde3e07accb5ea1d1fbe26119e123361ef37cc8
Steve Munoz <stevomunoz@yahoo.com>
2024-01-29 19:22:31 -0600

    updated about me sections.

--- pages/index.js
+++ pages/index.js
@@ -21,10 +21,14 @@ export default function Home({allPostsData}) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello, My name is Steve Munoz and I am an aspiring software engineer and enthustiac learner of all things. You can contact me through LinkedIn</p>
+        <p>Hello, My name is Steve Munoz and I am a passionate aspiring software engineer and enthustiac learner. Some of my favorite pastimes are hiking in nature and giving myself a mental break from the daily stresses of life. This includes kayaking, biking, and running. Catch me outside about that!</p>
         <p>
-          I am passionate about learning and writing. Please see a few of my blogs.
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.
+          Check out my 
+          <a href="https://samunoz.com"> portfolio </a>
+          and send me a message if you would like to connect.
+        </p>
+        <p>
+          You can find me on <a href="https://www.linkedin.com/in/stevemunoz">LinkedIn. </a>
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
